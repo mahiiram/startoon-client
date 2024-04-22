1efc714bd64de744ba5ce444adfe56f8adf10179
mahiiram <mahendran.ramar9797@gmail.com>
2024-04-22 10:15:56 +0530

    new commit

--- src/Pages/profile/UserProfile.js
+++ src/Pages/profile/UserProfile.js
@@ -9,33 +9,43 @@ function UserProfile() {
     const dispatch = useDispatch()
 
     useEffect(() => {
-        const token = localStorage.getItem('token'); 
+        const token = localStorage.getItem('token');
         axios.get('https://startoon-server-vbz8.onrender.com/api/getuser', {
             headers: {
                 Authorization: `Bearer ${token}`
             }
         })
-        .then(response => {
-            console.log(response)
-            setUser(response.data);
-        }).then(()=>{
-            dispatch(userAction.login())
-        })
-        .catch(error => {
-            console.error('Error fetching user:', error);
-        });
+            .then(response => {
+                console.log(response)
+                setUser(response.data);
+            }).then(() => {
+                dispatch(userAction.login())
+            })
+            .catch(error => {
+                console.error('Error fetching user:', error);
+            });
     }, []);
 
     return (
-        <div>
-            <Navbar/>
+        <div >
+            <Navbar />
             {user ? (
-                <div>
-                    <p>Name: {user.name}</p>
-                    <p>Email: {user.email}</p>
-                    <p>gender: {user.gender}</p>
-                    {/* Render other user information */}
+                // <div>
+                //     <p>Name: {user.name}</p>
+                //     <p>Email: {user.email}</p>
+                //     <p>gender: {user.gender}</p>
+
+                // </div>
+                <div className='userprofile'>
+                    <div class="card" style={{width:"18rem",height:"100%"}}>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Name: {user.name}</li>
+                        <li class="list-group-item">Email: {user.email}</li>
+                        <li class="list-group-item">gender: {user.gender}</li>
+                    </ul>
                 </div>
+                </div>
+                
             ) : (
                 <p>Loading user profile...</p>
             )}
